9b8812bf305a1fa811255d216e08ebc7c01090a1
sentieant <aravindsn002@gmail.com>
2024-05-17 20:13:06 +0530

    Resolved the ngModel issue T_T

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  login(){}
+  username: string = '';
+  password: string = '';
+
+  constructor() { }
+
+  login() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 }
