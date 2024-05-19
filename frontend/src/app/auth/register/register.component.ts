b3ee7754ba248f8f009db99c408e73577a6ad58e
sentieant <aravindsn002@gmail.com>
2024-05-20 01:11:32 +0530

    Partially made frontend for registration

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  username!: string;
+  password!: string;
 
+  constructor() { }
+
+  login() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 }
