0bc3f890ddb4cf9304bc89c804b11a090d23207a
sentieant <aravindsn002@gmail.com>
2024-05-27 22:02:13 +0530

    sql editor finished, both frontend and backend

--- frontend/src/app/sql-editor/sql-editor/sql-editor.component.ts
+++ frontend/src/app/sql-editor/sql-editor/sql-editor.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as ace from 'ace-builds';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-sql-editor',
@@ -7,6 +10,33 @@ import { Component } from '@angular/core';
   templateUrl: './sql-editor.component.html',
   styleUrl: './sql-editor.component.css'
 })
-export class SqlEditorComponent {
+export class SqlEditorComponent implements OnInit {
+  private editor!: ace.Ace.Editor;
+  private results: any;
+  private points!: number;
 
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.editor = ace.edit('editor');
+    this.editor.setTheme('ace/theme/monokai');
+    this.editor.session.setMode('ace/mode/sql');
+  }
+
+  runQuery() {
+    const sqlQuery = this.editor.getValue();
+    this.http.post<any>('api/query', {sql: sqlQuery}, {
+      headers: {
+        Authorization: `Bearer ${this.authService.getToken()}`
+      }
+    }).subscribe(
+      data => {
+        this.results = data;
+        alert('Query Executed!');
+      },
+      error => {
+        this.results = {error : error.message};
+      }
+      );
+  }
 }
